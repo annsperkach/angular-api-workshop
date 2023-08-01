75fb8d8420ae6d65d06321767cf0a4b97cc09481
Anna Sperkach <annsperkach@gmail.com>
2023-08-01 12:42:37 +0300

    adding main implementing of code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StarwarsService, character } from './starwars.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'api';
+
+  constructor(private readonly starWarsService: StarwarsService) {}
+
+  characters: any | null = null;
+  characters$: Observable<character[]> =
+    this.starWarsService.getAllCharacters3();
+
+  ngOnInit() {
+    this.characters = this.starWarsService.getAllCharacters();
+  }
 }
--- src/app/starwars.service.ts
+++ src/app/starwars.service.ts
@@ -1,9 +1,61 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, firstValueFrom, map, switchMap } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class StarwarsService {
-  constructor(private readonly http: HttpClient) {}
+  constructor(private readonly http: HttpClient) {
+    this.getAllCharacters().then((chars) => (this.characters = chars));
+  }
+
+  characters: character[] = [];
+
+  async getAllCharacters(): Promise<character[]> {
+    const response = await firstValueFrom(
+      this.http.get<starWarsResponse>(`${environment.api}`)
+    );
+
+    console.log('res', response);
+
+    return response.results;
+  }
+
+  getAllCharacters2(): Promise<character[]> {
+    const response = firstValueFrom(
+      this.http.get<starWarsResponse>(`${environment.api}`)
+    ).then((response) => {
+      return response.results;
+    });
+
+    console.log('res', response);
+
+    return Promise.resolve([]);
+  }
+
+  getAllCharacters3(): Observable<character[]> {
+    return this.http
+      .get<starWarsResponse>(`${environment.api}`)
+      .pipe(map((res) => res.results));
+  }
+}
+
+export interface starWarsResponse {
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: character[];
+}
+
+export interface character {
+  name: string;
+  height: string;
+  mass: string;
+  hair_color: string;
+  skin_color: string;
+  eye_color: string;
+  birth_year: string;
+  gender: string;
 }
